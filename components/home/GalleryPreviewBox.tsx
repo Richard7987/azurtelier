27b956242425e4be3e04946fed6f825f47a5d8a6
Negasonic7987 <alejandro_brnal@icloud.com>
2025-06-15 21:58:07 -0600

    fix

--- components/home/GalleryPreviewBox.tsx
+++ components/home/GalleryPreviewBox.tsx
@@ -2,12 +2,7 @@ import Photo from '@/components/gallery/photo'
 import { v4 as uuidv4 } from 'uuid'
 
 export default function GalleryPreviewBox() {
-  const imgSrc = [
-    '/static/gallery/art/ai/selfie/04E5B2.jpg',
-    '/static/gallery/art/ai/selfie/126348.jpg',
-    '/static/gallery/art/watercolor/12810.jpg',
-    '/static/gallery/art/watercolor/cb652.jpg',
-  ]
+  const imgSrc = ['/static/gallery/home12.jpeg']
 
   return (
     <div className={`card bg-pink-blue-animated sticky right-0 flex-grow overflow-hidden`}>
