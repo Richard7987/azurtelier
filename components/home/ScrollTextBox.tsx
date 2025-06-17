7bf840ec78b1bd617a46b50627b78912e8c28b5e
Negasonic7987 <alejandro_brnal@icloud.com>
2025-06-16 21:04:15 -0600

    Update ScrollTextBox.tsx

--- components/home/ScrollTextBox.tsx
+++ components/home/ScrollTextBox.tsx
@@ -12,7 +12,7 @@ export default function ScrollTextBox() {
     <div
       className={`card bg-pink-blue-animated animation-delay-2 flex-center flex-grow flex-col space-y-5 overflow-hidden p-2`}
     >
-      <h1 className="font-zzz2 text-white">Coming Soon</h1>
+      <h1 className="font-arial text-white">Coming Soon</h1>
       <motion.div className="bg-gray-purple h-8 w-8" style={{ scale, rotate, borderRadius }} />
     </div>
   )
